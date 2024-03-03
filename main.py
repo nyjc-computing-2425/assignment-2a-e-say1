ebf7189ed5de2d58422933655f3a12343829bb66
e-say1 <120434420+e-say1@users.noreply.github.com>
2024-03-03 06:26:25 +0000

    assignment 2a final submission

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 num = input('Enter a number (decimal or integer): ')
 # type your code here
+temp_num = num.strip()
+temp_num = temp_num.replace(".", "")
+temp_num = temp_num.lstrip("0")
+sf = len(temp_num)
+
 
 
 
 # do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
+
